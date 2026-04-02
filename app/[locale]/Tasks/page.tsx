3708e47508a7ae6a89d16384be3fa681111e812b
korbenian <sergeykorobov.frontend@gmail.com>
2026-04-02 13:57:26 +0500

    fix uncorrect import

--- app/[locale]/Tasks/page.tsx
+++ app/[locale]/Tasks/page.tsx
@@ -1,10 +1,6 @@
-import { useTranslations } from 'next-intl'
-import Sidebar from '../components/Sidebar'
 import AddTask from './addTasks'
-
-
 export default function TaskPage() {
-  const  t  = useTranslations()
+
 
   return (
   
